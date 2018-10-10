e12027b30b170e38dced9ff7072ce2adfb91fbab
Eugene_Chan <ilvinchan95@hotmail.com>
2018-10-10 17:53:07 +0900

    Update to connect database

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 
 from selenium import webdriver
-
+import pymysql
 #Get all info from link;
 
 options = webdriver.ChromeOptions()
@@ -13,7 +13,19 @@ driver = webdriver.Chrome("S:/Career/merukali_price/chromedriver.exe",options=op
 link = 'https://www.mercari.com/jp/brand/1620/'
 keyword = ''
 
+def connect():
+    server = '206.189.90.203'
+    database = 'merukali'
+    username = 'zun95'
+    password = '19951203'
+    db = pymysql.connect(server,username,password,database)
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
 
+    # 关闭数据库连接
+    db.close()
 def research():
     driver.get(link);
     #get all link
@@ -33,7 +45,9 @@ def research():
     #     driver.refresh()
     #     time.sleep(10)
     #     research()
-research()
+print("hi")
+connect()
+#research()
 
 # driver.quit()
 # quit()
