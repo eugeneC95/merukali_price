b1c90383a64b481446c8344488d105a38eb25996
Eugene_Chan <ilvinchan95@hotmail.com>
2018-10-12 00:17:31 +0900

    New update on infinite loop

--- reinput.py
+++ reinput.py
@@ -1,7 +1,6 @@
 import pymysql
 from selenium.common.exceptions import NoSuchElementException
 import time
-
 db = pymysql.connect(host='206.189.90.203',user='zun95',passwd='Hotdilvin95',db='merucali')
 cursor = db.cursor()
 def inserts(hre,tit,lik,sto,pri,tim):
@@ -11,13 +10,14 @@ def inserts(hre,tit,lik,sto,pri,tim):
     except:
         print("error")
         db.rollback()
-def get(x):
+def get(x,h):
     try:
         cursor.execute("SELECT * FROM post WHERE href LIKE %s ORDER BY time_update ASC",(x))
         datas = cursor.fetchall()
         if len(datas) > 1:
             #main reinput
             for data in datas:
+
                 link = data[1]
                 title = data[2]
                 likes = data[3]
@@ -25,27 +25,30 @@ def get(x):
                 prices = data[5]
                 time = data[6]
                 #print(link,title,likes,stock,prices,time)
-                cursor.execute("SELECT * FROM changes WHERE href LIKE %s AND title LIKE %s AND likes LIKE %s AND stock LIKE %s AND price LIKE %s",(link,title,likes,stock,prices,time))
+                cursor.execute("SELECT * FROM changes WHERE href LIKE %s AND title LIKE %s AND likes LIKE %s AND stock LIKE %s AND price LIKE %s",(link,title,likes,stock,prices))
                 datos = cursor.fetchall()
                 if len(datos) >= 1:
-                    print("base",title,likes,stock,prices,time)
-                    print("checking")
+                    print("data alaready haved")
                     break
                 else:
                     inserts(link,title,likes,stock,prices,time)
-                    print("insert new data to table\n")
+                    print("insert new data to table")
         else:
+
             #donothings
-            print("none duplicate")
+            print(str(h),"none duplicate")
     except:
         print ("Errorget: unable to fetch data")
 def main():
     try:
         cursor.execute("SELECT * FROM post ")
         datas = cursor.fetchall()
+        h = 0
         for data in datas:
             link = data[1]
-            get(link)
+            h += 1
+            get(link,h)
+        main()
     except:
         print ("Errormain: unable to fetch data")
 
